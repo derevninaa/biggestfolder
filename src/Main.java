4f6d0c8607ef4a7fcbb382a718287e780eae6c28
@derevninaa <a.a.derevnin@gmail.com>
2020-07-25 14:45:22 +0300

    add HumanReadable

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,35 @@
 import java.io.File;
 import java.io.IOException;
-import java.nio.file.Files;
+import java.util.concurrent.ForkJoinPool;
 
 public class Main {
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         String folderPath = "/Users/derevnin/Desktop/";
         File file = new File(folderPath);
-        System.out.println(getFolderSize(file));
+        FolderSizeCalculator calculator = new FolderSizeCalculator(file);
+        ForkJoinPool pool = new ForkJoinPool();
+        long sum = pool.invoke(calculator);
+        System.out.println(humanReadable(sum));
     }
 
-    public static long getFolderSize(File file) throws IOException {
-        return Files.walk(file.toPath())
-            .filter(f -> f.toFile().isFile())
-            .mapToLong(f -> f.toFile().length())
-            .sum();
+    public static String humanReadable(long size){
+        double BYTE = 0.001;
+        double KB = 0.000001;
+        double MB = 0.000000001;
+        double GB = 0.000000000001;
+
+        String result = "";
+
+        if ((int) (size * BYTE) == 0) {
+            result = (size + " B");
+        } else if ((int) (size * KB) == 0) {
+            result += (size * BYTE) + " KB";
+        } else if ((int) (size * MB) == 0) {
+            result += (size * KB) + " MB";
+        } else if ((int) (size * GB) == 0) {
+            result += (size * MB) + " GB";
+        }
+        return result;
     }
 }
